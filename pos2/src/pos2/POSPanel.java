c823273e70ceb89e5ba2fb246b8e2826cf96b89b
Yewon918 <yewon918@naver.com>
2019-11-23 14:36:09 +0900

    소스설명본

--- pos2/src/pos2/POSPanel.java
+++ pos2/src/pos2/POSPanel.java
@@ -6,30 +6,35 @@ import javax.swing.*;
 import javax.swing.table.*;
 
 public class POSPanel extends JPanel {
-	JButton[] MBtn = new JButton[9];
-	String[] menu = {"순 대 국 밥","수 육 국 밥","뼈 해 장 국","돼 지 국 밥","콜 라","사 이 다","환 타","소 주","맥 주","판 매 관 리"
-	         };
+	JButton[] MBtn = new JButton[9]; //버튼 생성
+	String[] menu = {"순 대 국 밥","수 육 국 밥","뼈 해 장 국",
+			"돼 지 국 밥","콜 라","사 이 다",
+			"환 타","소 주","맥 주",
+			 };
 	
-	int[] price = {100, 200, 300, 400, 500, 600 ,700, 800, 900		
-	};
-	JTextField tf = new JTextField(20);
+	int[] price = {100, 200, 300, 
+			400, 500, 600,
+			700, 800, 900};
+	
+	JTextField tf = new JTextField(20); //인풋박스tf- 20
 	
 	JButton[] MGRtn = new JButton[1];
-	String[] MGR = {"�뙋留ㅺ�由�"};
+	//String[] MGR = {"판매관리"};
 	
-	JTextField t = new JTextField(10);
+	JTextField t = new JTextField(10); //인풋박스t- 입력창 열 10
 
 	JButton[] SBtn = new JButton[4];
-	String[] Str = {"荑좏룿","遺�遺꾩랬�냼","�쟾泥댁랬�냼","寃곗젣"};
+	String[] Str = {"쿠폰","부분취소","전체취소","결제"};
 	
-	String [] ColName = {"硫붾돱","�닔�웾","媛�寃�"};
+	String [] ColName = {"메뉴","수량","가격"};
 	
-	String [][] Data ;
+	String [][] Data;
 	int count =1;
 	DefaultTableModel model = new DefaultTableModel(Data,ColName);
-	JTable table = new JTable(model);
+	//Data와 Col에 연결된 상태
+	JTable table = new JTable(model); //DefaultTableModel로 테이블 관리
 	
-	class Screen extends JPanel{
+	class Screen extends JPanel{ //메뉴, 수량, 가격 
 		Screen(){
 			setBackground(Color.RED);
 			DefaultTableModel m = (DefaultTableModel)table.getModel();
@@ -37,32 +42,37 @@ public class POSPanel extends JPanel {
 			table.getTableHeader().setFont(new Font("궁서", Font.BOLD, 15));
 			add(new JScrollPane(table));
 		}
-	} //踰꾪듉�깮�꽦, 諛곌꼍�깋 �꽕�젙
+	} 
 	
+	//메뉴버튼을 생성함
 	class MenuBtn extends JPanel{
+		//여기 음료랑 먹을거 분리해서 할 수 있지 않을까
 		MenuBtn(){
 			setLayout(new GridLayout(3,3,3,3));
-			setBackground(Color.YELLOW);
+			//setLayout - 배치관리자 Grid는 격자모양 배치
+			//gridlayout(row,col,hgap,vgap) //gap-여백조절
+			setBackground(Color.YELLOW); //버튼 칸의 배경 설정
 			for(int i=0;i<MBtn.length;i++) {
 				MBtn[i]= new JButton(menu[i]);
-				add(MBtn[i]);
+				add(MBtn[i]); //최종적으로 버튼을 생성
 			}
 		}
-	} //硫붾돱紐� 踰꾪듉 �젅�씠�븘�썐�꽕�젙, �끂��遺�遺�
+	}
 	
-	
+	//판매관리 버튼 만듦
 	class MGRBtn extends JPanel{
 		MGRBtn(){
 			setBackground(Color.WHITE);
-			setLayout(new GridLayout(1, 1, 1, 1));
-			
-			for(int i=0;i<MGR.length;i++) {
+			setLayout(new GridLayout(1, 1, 1, 1));	
+			/*for(int i=0;i<MGR.length;i++) {
 				MGRtn[i]= new JButton(MGR[i]);
 				add(MGRtn[i]);
-			}
+			}*/
+			add(new JButton("판매관리")); //이렇게도 바꿀 수 있을것 같아요
 		}
-	}//�뙋留ㅺ�由щ쾭�듉 �젅�씠�븘�썐 �꽕�젙
+	}
 	
+	//쿠폰 ~ 결제 창 버튼
 	class StrBtn extends JPanel{
 		StrBtn(){
 			setBackground(Color.ORANGE);
@@ -71,89 +81,99 @@ public class POSPanel extends JPanel {
 			for(int i=0;i<Str.length;i++) {
 				SBtn[i]= new JButton(Str[i]);
 				add(SBtn[i]);
+				//add(new JButton(Str[i])); - 똑같은건데 이거 사용하면 예외처리 뜸
 			}
 		}
-	} //荑좏룿~寃곗젣 踰꾪듉 �젅�씠�븘�썐�꽕�젙, 諛곌꼍 二쇳솴遺�遺�
+	}
 	
+	//기본패널
 	public POSPanel() {
 		setLayout(null);
-		setBackground(Color.GREEN);
+		setBackground(Color.GREEN); 
 		MenuBtn mbtn = new MenuBtn();
 		MGRBtn mgrtn = new MGRBtn();
 		StrBtn sbtn = new StrBtn(); //踰꾪듉諛곗뿴
 		
-		Screen sc = new Screen();
+		Screen sc = new Screen(); //사용자의 화면을 나타냄
 		
 		//�쟾泥댄솕硫� �뀒�몢由� �젅�씠�븘�썐, 珥덈줉�깋�
-		tf.setSize(450, 70);
-		tf.setLocation(50, 480);
+		tf.setSize(450, 70); //가격 표시 창
+		tf.setLocation(50, 480); //가로 위치, 세로 위치
 		add(tf);
 		
-		sc.setSize(500, 500); 
+		sc.setSize(500, 500); //메뉴 선택시 선택 메뉴 뜨는 창
 		sc.setLocation(25, 20);
 		add(sc);
 		
-		mbtn.setSize(400, 350); //硫붾돱踰꾪듉 �궗�씠利� (媛�濡�, �꽭濡�)
-		mbtn.setLocation(530, 23); //硫붾돱踰꾪듉 �쐞移� 
+		mbtn.setSize(400, 350); //메뉴버튼 사이즈(가로, 세로)
+		mbtn.setLocation(530, 23);
 		add(mbtn);
 		
-		mgrtn.setSize(400, 70); //愿�由щ쾭�듉
+		mgrtn.setSize(400, 70); //관리 버튼
 		mgrtn.setLocation(530, 400);
 		add(mgrtn);
 		
-		sbtn.setSize(400, 70); //寃곗젣踰꾪듉
+		sbtn.setSize(400, 70); //쿠폰 등 버튼
 		sbtn.setLocation(530, 480);
 		add(sbtn);
 		
-
-		for(int i=0;i<MBtn.length;i++) {
+		//Table 관리
+		//메뉴버튼-추가
+		for(int i=0;i<MBtn.length;i++) { //0~8까지 
 			final int index =i;
 			MBtn[i].addActionListener(new ActionListener() {
+				//ActionListener은 버튼이 클릭될때마다 발생하는 이벤트를 만듦
+				//버튼에 액션 리스너를 추가한다
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					JButton MBtn = (JButton)e.getSource();
+					//JButton MBtn = (JButton)e.getSource();
+					//왜있는건지 이해 안됨
 					DefaultTableModel m = (DefaultTableModel)table.getModel();
 					m.addRow(new Object[]{menu[index],count,price[index]});
+					//선택된 메뉴 추가(추가되는 데이터 내용) -count 건드리면 저번에 말한 내용 가능
 				}
 			});
 		}
 		
+		//쿠폰 버튼-가격 0으로
 		SBtn[0].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JButton MBtn = (JButton)e.getSource();
+				//JButton MBtn = (JButton)e.getSource();
 				table.setValueAt(0, table.getSelectedRow(), 2);
+				//col2의 값을 0으로
 			}
 		});
 		
-			
+		//부분취소 버튼
 		SBtn[1].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JButton MBtn = (JButton)e.getSource();
+				//JButton MBtn = (JButton)e.getSource();
 				DefaultTableModel m = (DefaultTableModel)table.getModel();
 				
 				m.removeRow(table.getSelectedRow());
+				//선택된 row를 없앰 
 			}
 		});
 		
-		
+		//전체취소
 		SBtn[2].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JButton MBtn = (JButton)e.getSource();
+				//JButton MBtn = (JButton)e.getSource();
 				DefaultTableModel m = (DefaultTableModel)table.getModel();
 				
-				m.setRowCount(0);
+				m.setRowCount(0); //모델 내의 행수 설정
 				tf.setText(String.valueOf(""));
 			}
 		});
 		
-		
+		//결제
 		SBtn[3].addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JButton MBtn = (JButton)e.getSource();
+				//JButton MBtn = (JButton)e.getSource();
 				int rowCont = table.getRowCount();
 				int sum =0;
 				for(int i=0;i<rowCont;i++) {
--- pos2/src/pos2/POS_Frame.java
+++ pos2/src/pos2/POS_Frame.java
@@ -1,15 +1,20 @@
 package pos2;
 
 import javax.swing.JFrame;
-
+//awt는 실행환경에 따라 컴포넌트 모습이 달라져 일관된 화면 제공이 힘듦- swing권장
 public class POS_Frame extends JFrame {
 	public POS_Frame() {
 		setTitle("뜨거워 죽어도 국밥");
+		//setLocationRelativeTo(null); //-가운데오도록함 default와 따라다님
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Exit_on_close : 윈도우 창 종료시 프로세스까지 닫음
+		//이거 안하면 실제로는 프로세스가 계속 돌아가고, 메모리 잡아먹음
+		setContentPane(new POSPanel());		
+		//(이렇게도 가능하다)POSPanel pos = new POSPanel();
+		//setContentPane(pos);
 		
-		setContentPane(new POSPanel());
-
 		setSize(985,660);
 		setVisible(true);
+		//보이게 함
 	}
 }
